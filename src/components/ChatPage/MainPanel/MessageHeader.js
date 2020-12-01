a763f3eb77e4d2238cfb91b773d25e0b5b61b320
minsun21 <mspark0429@daum.net>
2020-12-01 23:06:44 +0900

    Favorited 구현

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import firebase from '../../../firebase';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -13,20 +14,70 @@ import Button from 'react-bootstrap/Button';
 
 import { FaLock, FaLockOpen } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { MdFavorite } from 'react-icons/md';
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 
 function MessageHeader({ handleSearchChange }) {
+    const user = useSelector(state => state.user.currentUser);
     const chatRoom = useSelector(state => state.chatRoom.currentChatRoom);
     const isPrivateChatRoom = useSelector(state => state.chatRoom.isPrivateChatRoom);
+    const [isFavorited, setisFavorited] = useState(false);
+    const usersRef = firebase.database().ref("users");
+
+    useEffect(() => {
+        if (chatRoom && user)
+            addFavoriteListener(chatRoom.id, user.uid);
+    }, [])
+    const addFavoriteListener = (chatRoomId, userId) => {
+        usersRef.child(userId).child("favorited").once("value").then(data => {
+            if (data.val() !== null) {
+                const chatRoomIds = Object.keys(data.val());
+                const isAlreadyFavorited = chatRoomIds.includes(chatRoomId);
+                setisFavorited(isAlreadyFavorited);
+            }
+        })
+    }
+
+    const handleFavorite = () => {
+        if (isFavorited) {
+            usersRef.child(`${user.uid}/favorited`).child(chatRoom.id).remove(err => {
+                if (err != null) {
+                    console.log(err);
+                }
+            })
+            setisFavorited(prev => !prev);
+        } else {
+            usersRef.child(`${user.uid}/favorited`).update({
+                [chatRoom.id]: {
+                    name: chatRoom.name,
+                    desc: chatRoom.desc,
+                    createdBy: {
+                        name: chatRoom.name,
+                        image: chatRoom.createdBy.image
+                    }
+                }
+            })
+            setisFavorited(prev => !prev);
+        }
+    }
     return (
         <div className="main-panel__message-header">
             <Container>
                 <Row>
-                    <Col><h2>
-                        {isPrivateChatRoom ? <FaLock style={{ marginRight: '3px' }} /> : <FaLockOpen style={{ marginRight: '3px' }} />}
-                        {chatRoom && chatRoom.name}
-
-                        <MdFavorite /></h2></Col>
+                    <Col>
+                        <h2>
+                            {isPrivateChatRoom ? <FaLock style={{ marginRight: '3px' }} /> : <FaLockOpen style={{ marginRight: '3px' }} />}
+                            {chatRoom && chatRoom.name}
+                            {!isPrivateChatRoom &&
+                                <span style={{ cursor: 'pointer' }} onClick={handleFavorite}>
+                                    {isFavorited ?
+                                        <MdFavorite style={{ marginBottom: '10px' }} />
+                                        :
+                                        <MdFavoriteBorder style={{ marginBottom: '10px' }} />
+                                    }
+                                </span>
+                            }
+                        </h2>
+                    </Col>
                     <Col>
                         <InputGroup className="mb-3">
                             <InputGroup.Prepend>
--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -1,11 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { setCurrentChatRoom, setPrivateChatRoom } from '../../../redux/actions/chatRoom_action';
+import { FaRegSmileBeam } from 'react-icons/fa';
+import firebase from '../../../firebase';
 
-function Favorited() {
-    return (
-        <div>
-            Favorited
-        </div>
-    )
+
+
+export class Favorited extends Component {
+    state = {
+        favoritedChatRooms: [],
+        usersRef: firebase.database().ref("users"),
+        activeChatRoomId: ''
+    }
+
+    componentDidMount() {
+        if (this.props.user)
+            this.addListeners(this.props.user.uid);
+    }
+
+    componentWillUnmount() {
+        if (this.props.user) {
+            this.removeListener(this.props.user.uid);
+        }
+    }
+    removeListener = (userId) => {
+        this.state.usersRef.child(`${userId}/favorited`).off();
+    }
+
+    addListeners = (userId) => {
+        const { usersRef } = this.state;
+        usersRef
+            .child(userId)
+            .child("favorited")
+            .on("child_added", DataSnapshot => {
+                const favoritedChatRoom = { id: DataSnapshot.key, ...DataSnapshot.val() }
+                this.setState({
+                    favoritedChatRooms: [...this.state.favoritedChatRooms, favoritedChatRoom]
+                })
+            });
+        usersRef
+            .child(userId)
+            .child("favorited")
+            .on("child_removed", DataSnapshot => {
+                const chatRoomToRemove = { id: DataSnapshot.key, ...DataSnapshot.val() }
+                const filtedChatRooms = this.state.favoritedChatRooms.filter(chatRoom => {
+                    return chatRoom.id !== chatRoomToRemove.id;
+                })
+                this.setState({
+                    favoritedChatRooms: filtedChatRooms
+                })
+            });
+    }
+
+    changeChatRoom = (room) => {
+        this.props.dispatch(setCurrentChatRoom(room));
+        this.props.dispatch(setPrivateChatRoom(false));
+        this.setState({ activeChatRoomId: room.id });
+    }
+
+    renderFavoritedChatRooms = favoritedChatRooms =>
+        favoritedChatRooms.length > 0 &&
+        favoritedChatRooms.map(chatRoom => (<li style={{ cursor: 'pointer' }} key={chatRoom.id} onClick={() => this.changeChatRoom(chatRoom)} className={chatRoom.id === this.state.activeChatRoomId && 'active'}>{chatRoom.name}</li>))
+
+    render() {
+        const { favoritedChatRooms } = this.state;
+        return (
+            <div className="favorited-panel">
+                <span>
+                    <FaRegSmileBeam style={{ marginRight: '3px' }} />
+                FAVORITED ({favoritedChatRooms.length})
+            </span>
+                <ul>
+                    {this.renderFavoritedChatRooms(favoritedChatRooms)}
+                </ul>
+            </div>
+        )
+    }
 }
-
-export default Favorited
+const mapStateToProps = state => {
+    return {
+        user: state.user.currentUser
+    }
+}
+export default connect(mapStateToProps)(Favorited)
